70653f6f3b7440389a19941610586caf44428f77
nibble0101 <mj.mawa@yahoo.com>
2021-05-08 23:04:30 +0300

    Add flag if nominations reach limit

--- src/components/main/search/SearchResult.js
+++ src/components/main/search/SearchResult.js
@@ -12,6 +12,13 @@ export default function SearchResults(props) {
   }
   return (
     <div className="search-result">
+      <div className="warning-wrapper">
+        {props.nominations.length === 5 ? (
+          <p className="warning">
+            You have reached nominations limit of 5 movies
+          </p>
+        ) : null}
+      </div>
       <h2 className="search-result__title">Movie search result</h2>
       <div className="search-result__list">
         {props.movies.map((movie, index) => (
@@ -19,7 +26,7 @@ export default function SearchResults(props) {
             movie={movie}
             nominateMovie={props.nominateMovie}
             rateMovie={props.rateMovie}
-            key={`${movie.imdbID}_${index}` }
+            key={`${movie.imdbID}_${index}`}
             nominations={props.nominations}
           />
         ))}
