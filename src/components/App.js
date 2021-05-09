6ea3fe7de26f8abc12999ab923bee00ef2400993
nibble0101 <mj.mawa@yahoo.com>
2021-05-09 22:26:25 +0300

    Fix non responsiveness of form input element for small screens

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Main from "./main/Main";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-import {BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "../styles/App.css";
 
 function App() {
