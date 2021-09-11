05862bb59e2dcd55f0ce219d9d07081937ceb5a7
nibble0101 <mj.mawa@yahoo.com>
2021-09-11 23:14:28 +0300

    Add netlify functions

--- netlify/functions/movies/movies.js
+++ netlify/functions/movies/movies.js
@@ -1,22 +1,38 @@
-const baseUrl = "https://www.omdbapi.com";
 const axios = require("axios");
+
+const baseUrl = "https://www.omdbapi.com";
 
 const handler = async (event) => {
   try {
     const { query } = event.queryStringParameters;
-    if(!query){
-      throw new Error("Missing query string");
+    if (!query) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          Response: "False",
+          Error: "Missing query string",
+        }),
+      };
     }
-    const url = `${baseUrl}/?apikey=${process.env.API_KEY}&type=movie&s=${query}&page=1`
-    const movies = axios.get(url).data;
+    const url = `${baseUrl}/?apikey=${process.env.REACT_APP_API_KEY}&type=movie&s=${query}&page=1`;
+    const response = (await axios.get(url)).data;
+    if (response.hasOwnProperty("Error")) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(response),
+      };
+    }
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ movies: movies.Search }),
-     
-    }
+      body: JSON.stringify(response),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ Response: "False", Error: "Internal server error" }),
+    };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
